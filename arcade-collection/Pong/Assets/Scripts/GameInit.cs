65190c2ad603379b400753bcfa97c6d17094baa5
Nikita <34914008+nikitafin@users.noreply.github.com>
2021-04-24 17:37:15 +0300

    add basic touch control

--- arcade-collection/Pong/Assets/Scripts/GameInit.cs
+++ arcade-collection/Pong/Assets/Scripts/GameInit.cs
@@ -8,5 +8,11 @@ public class GameInit : MonoBehaviour
     private void Awake()
     {
         Stuff.Init();
+        // Screen.SetResolution(Screen.width, (int) Screen.height, true);
+    }
+
+    private void Start()
+    {
+        // Screen.SetResolution(Screen.width, Screen.height, true);
     }
 }
